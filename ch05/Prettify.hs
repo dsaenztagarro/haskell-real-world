2b7276135629b865ddb23011b1f5b856a60ea67b
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-01-12 00:23:40 +0100

    Added pretty printing

--- ch05/Prettify.hs
+++ ch05/Prettify.hs
@@ -66,9 +66,33 @@ compact x = transform [x]
     where transform [] = ""
           transform (d:ds) =
               case d of
-                Empty -> transform ds
-                Char c -> c : transform ds
-                Text s -> s ++ transform ds
-                Line -> '\n' : transform ds
+                Empty        -> transform ds
+                Char c       -> c : transform ds
+                Text s       -> s ++ transform ds
+                Line         -> '\n' : transform ds
                 a `Concat` b -> transform (a:b:ds)
-                _ `Union` b -> transform (b:ds)
+                _ `Union` b  -> transform (b:ds)
+
+pretty :: Int -> Doc -> String
+pretty width x = best 0 [x]
+		where
+				best col (d:ds) =
+	 					case d of
+			 				Empty        -> best col ds
+			 				Char c       -> c : best (col + 1) ds
+							Text s       -> s ++ best (col + length s) ds
+							Line         -> '\n' : best 0 ds
+ 							a `Concat` b -> best col (a:b:ds)
+ 							a `Union` b  -> nicest col (best col (a:ds))
+ 																				 (best col (b:ds))
+				best _ _ = ""
+
+				nicest col a b | (width - least) `fits` a = a
+											 | otherwise = b
+											 where least = min width col
+
+fits :: Int -> String -> Bool
+w `fits` _ | w < 0 = False
+_ `fits` ""        = True
+_ `fits` ('\n':_)  = True
+w `fits` (_:cs)    = (w - 1) `fits` cs
