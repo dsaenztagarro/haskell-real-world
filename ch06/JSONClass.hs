e852f85d2a221441cc3d6685011f3c941ae8b6ba
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-01-20 06:56:45 +0100

    Added cabal package example. Added typeclasses intro

---
+++ ch06/JSONClass.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
+type JSONError = String
+
+class JSON a where
+  toJValue :: a -> JValue
+  fromJValue :: JValue -> Either JSONError a
+
+instance JSON JValue where
+  toJValue = id
+  fromJValue = Right
+
+instance JSON Bool where
+  toJValue = JString
+  fromJValue (JBool b) = Right b
+  fromJValue _ = Left "not a JSON string"
+
+instance JSON String where
+  toJValue = JString
+  fromJValue (JString s) = Right s
+  fromJValue _ = Left "not a JSON string"
