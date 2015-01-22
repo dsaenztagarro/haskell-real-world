de4c3991e0d522b97e38c6697dc8f24eb3be1aac
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-01-23 00:40:27 +0100

    Added tempfile

---
+++ ch07/tempfile.hs
@@ -0,0 +1,43 @@
+import Prelude hiding(catch)
+import System.IO
+import System.Directory(getTemporaryDirectory, removeFile)
+import Control.Exception(catch, finally)
+
+main :: IO ()
+main = withTempFile "mytemp.txt" myAction
+
+myAction :: FilePath -> Handle -> IO ()
+myAction tempname temph = do
+    putStrLn "Welcome to tempfile.hs"
+    putStrLn $ "I have a temporary file at " ++ tempname
+
+    pos <- hTell temph
+    putStrLn $ "My initial position is " ++ show pos
+
+    let tempdata = show [1..10]
+    putStrLn $ "Writing one line containing " ++
+               show (length tempdata) ++ " bytes: " ++ tempdata
+    hPutStrLn temph tempdata
+
+    pos <- hTell temph
+    putStrLn $ "After writing, my new position is " ++ show pos
+
+    putStrLn $ "The file content is: "
+    hSeek temph AbsoluteSeek 0
+
+    -- hGetContents performs a lazy read of the entire file
+    c <- hGetContents temph
+    putStrLn c
+    putStrLn "Which could be expressed as this Haskell literal:"
+    print c
+
+withTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
+withTempFile pattern func = do
+    tempdir <- catch (getTemporaryDirectory) (\_ -> return ".")
+    -- let tempdir = "."
+    (tempfile, temph) <- openTempFile tempdir pattern
+
+    finally (func tempfile temph)
+            (do hClose temph
+                removeFile tempfile)
+
