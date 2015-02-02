9de922fa75da5c3cc061de1ab0b6b0016acdecce
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-02-02 07:51:55 +0100

    Fixed walk to traverse recursively subdirectories with valid parent path

--- ch09/FoldDir.hs
+++ ch09/FoldDir.hs
@@ -24,22 +24,22 @@ foldTree iter initSeed path = do
     endSeed <- fold initSeed path
     return (unwrap endSeed)
   where
-    fold seed subpath = getUsefulContents subpath >>= walk seed
+    fold seed subpath = getUsefulContents subpath >>= walk seed subpath
 
-    walk seed (name:names) = do
+    walk seed path (name:names) = do
       let path' = path </> name
       info <- getInfo path'
       case iter seed info of
         done@(Done _) -> return done
-        Skip seed' -> walk seed' names
+        Skip seed' -> walk seed' path names
         Continue seed'
           | isDirectory info -> do
               next <- fold seed' path'
               case next of
                 done@(Done _) -> return done
-                seed''        -> walk (unwrap seed'') names
-          | otherwise -> walk seed' names
-    walk seed _ = return (Continue seed)
+                seed''        -> walk (unwrap seed'') path names
+          | otherwise -> walk seed' path names
+    walk seed _ _ = return (Continue seed)
 
 
 atMostThreePictures :: Iterator [FilePath]
