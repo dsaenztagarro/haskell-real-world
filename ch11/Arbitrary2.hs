652695292a373fe091944670e12daee25843bafb
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-02-09 08:25:37 +0100

    Testing document construction

---
+++ ch11/Arbitrary2.hs
@@ -0,0 +1,7 @@
+instance Arbitrary Ternary where
+  arbitrary = do
+    n <- choose (0, 2) :: Gen Int
+    return $ case n of
+                  0 -> Yes
+                  1 -> No
+                  _ -> Unknown
---
+++ ch11/QC.hs
@@ -0,0 +1,50 @@
+import Prettify2
+import Control.Monad (liftM, liftM2)
+import Test.QuickCheck
+
+-- instance Arbitrary Char where
+--     arbitrary = elements (['A'..'Z'] ++ ['a'..'z'] ++ " ˜!@#$%^&*()")
+
+-- instance Arbitrary Doc where
+--     arbitrary = do
+--       n <- choose (1,6) :: Gen Int
+--       case n of
+--           1 -> return Empty
+--
+--           2 -> do x <- arbitrary
+--                   return (Char x)
+--
+--           3 -> do x <- arbitrary
+--                   return (Text x)
+--
+--           4 -> return Line
+--
+--           5 -> do x <- arbitrary
+--                   y <- arbitrary
+--                   return (Concat x y)
+--
+--           6 -> do x <- arbitrary
+--                   y <- arbitrary
+--                   return (Union x y)
+
+instance Arbitrary Doc where
+    arbitrary = do
+        oneof [ return Empty
+              , liftM Char arbitrary
+              , liftM Text arbitrary
+              , return Line
+              , liftM2 Concat arbitrary arbitrary
+              , liftM2 Union arbitrary arbitrary ]
+
+prop_empty_id x =
+    empty <> x == x
+  &&
+    x <> empty == x
+
+prop_char c = char c == Char c
+
+prop_text s == if null s then Empty else Text s
+
+prop_line = line == Line
+
+prop_double d = double d == text (show d)
