f26f0cd07ddae1448edd6b5276a03441f240ef96
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-02-12 22:06:42 +0100

    Added batch test runner

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Haskell Real World
 
 All exercises have been modified in order to run with Haskell 2014
+
+```shell
+$ cabal install QuickCheck-1.2.0.1
+$ ghc-pkg expose QuickCheck-1.2.0.1
+$ ghc-pkg hide QuickCheck-2.7.*
+$ ghc-pkg list | grep QuickCheck
+```
+
+Running tests
+
+```shell
+ghc --make Run.hs
+./Run
+```
--- ch11/QC.hs
+++ ch11/QC.hs
@@ -1,9 +1,13 @@
+module QC where
+
 import Prettify2
-import Control.Monad (liftM, liftM2)
 import Test.QuickCheck
 
--- instance Arbitrary Char where
---     arbitrary = elements (['A'..'Z'] ++ ['a'..'z'] ++ " ˜!@#$%^&*()")
+import Control.Monad (liftM, liftM2)
+import Data.List (intersperse)
+
+instance Arbitrary Char where
+    arbitrary = elements (['A'..'Z'] ++ ['a'..'z'] ++ " ˜!@#$%^&*()")
 
 -- instance Arbitrary Doc where
 --     arbitrary = do
@@ -43,8 +47,23 @@ prop_empty_id x =
 
 prop_char c = char c == Char c
 
-prop_text s == if null s then Empty else Text s
+prop_text s = text s == if null s then Empty else Text s
 
 prop_line = line == Line
 
 prop_double d = double d == text (show d)
+
+prop_hcat xs = hcat xs == glue xs
+    where glue [] = empty
+          glue (d:ds) = d <> glue ds
+
+prop_punctuate s xs = punctuate s xs == intersperse s xs
+
+prop_punctuate' s xs = punctuate s xs == combine (intersperse s xs)
+  where
+    combine [] = []
+    combine [x] = [x]
+
+    combine (x:Empty:ys) = x : combine ys
+    combine (Empty:y:ys) = y : combine ys
+    combine (x:y:ys) = x `Concat` y : combine ys
